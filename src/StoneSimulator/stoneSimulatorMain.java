8d6bf88306dfdd981374b91882a7d96337243847
changju Park <101298499+ds4pae@users.noreply.github.com>
2023-06-02 20:24:02 +0900

    Update stoneSimulatorMain.java

--- src/StoneSimulator/stoneSimulatorMain.java
+++ src/StoneSimulator/stoneSimulatorMain.java
@@ -3,7 +3,6 @@ package StoneSimulator;
 public class stoneSimulatorMain {
 
 	public static void main(String[] args) {
-        // GUI Ω√¿€
     	stoneSimulator gui = new stoneSimulator();
         gui.createAndShowGUI();
     }
